2320df3c2d1355f72e871913c5ef14be51afae43
akashprajapati9548 <akp954834@gmail.com>
2025-06-08 06:04:10 +0530

    Update start.py

--- SACHINxSPAM/modules/start.py
+++ SACHINxSPAM/modules/start.py
@@ -1,21 +1,21 @@
-# ©️ @SACHIN_OWNER || @V_VIP_OWNER
+# ©️ @WTF_NoHope || @WTF_NoHope
 from telethon import __version__, events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_OP = [
     [
-        Button.url("🍁 sᴀᴄʜɪɴ", "https://t.me/V_VIP_OWNER"),
-        Button.url("ᴜsᴇʀʙᴏᴛ 🕸️", "https://t.me/SANATANI_X_ROBOT")
+        Button.url("🍁 AKASH", "https://t.me/Akash_Daksh1c"),
+        Button.url("MUSIC BOT 🕸️", "https://t.me/AD_Creation_Chatzone")
     ],
     [
         Button.inline("🥀 ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅs 🥀", data="help_back")
     ],
     [
-        Button.url("✨ ᴜᴘᴅᴀᴛᴇ", "https://t.me/All_SANATANI_BOT"),
-        Button.url("sᴜᴘᴘᴏʀᴛ ❄️", "https://t.me/+Ckzm2ypQyIIzZTll")
+        Button.url("✨ ᴜᴘᴅᴀᴛᴇ", "https://t.me/Sanatani_Network"),
+        Button.url("sᴜᴘᴘᴏʀᴛ ❄️", "https://t.me/AD_Creation_Chatzone")
     ],
     [
-        Button.url("🌸 ᴊᴏɪɴ ғᴏʀ sᴜᴅᴏ 🌸", "https://t.me/+Ckzm2ypQyIIzZTll")
+        Button.url("🌸 ᴊᴏɪɴ ғᴏʀ sᴜᴅᴏ 🌸", "https://t.me/AD_Creation_Chatzone")
     ],
 ]
 
@@ -34,8 +34,8 @@ async def start(event):
         KEX = await event.client.get_me()
         bot_name = KEX.first_name
         bot_id = KEX.id
-        TEXT = f"**╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ⏤͟͟͞͞‌‌‌‌★**\n**┆**\n**┊◍ ʜᴇʏ : [{event.sender.first_name}] **\n**┆◍ ɪ ᴀᴍ : [{bot_name}](tg://user?id={bot_id}) **\n**┊**\n**┆● sᴀɴᴀᴛᴀɴɪ ʙᴏᴛ ᴠᴇʀsɪᴏɴ :** `0.2`\n**┊● ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `8.2.5.1.01`\n**╰─────────────────────────**\n**──────────────────────────**\n**⦿ Oᴡɴᴇʀ - [🦋⃟‌🇸ʌᷟᴄᷣʜɪ֟፝η 🌸](https://t.me/v_vip_owner) | [🦋⃟‌🇸ʌᷟᴄᷣʜɪ֟፝η 🌸](https://t.me/sachin_owner) **\n**──────────────────────────**\n**    ❖ Uᴘᴅᴀᴛᴇ's ⏤͟͟͞͞‌‌‌‌ [❖ ∣ Sᴀɴᴀᴛᴀɴɪ Tᴇᴄʜ ∣ ❖](https://t.me/all_sanatani_bot) **\n**──────────────────────────**"
-        await event.client.send_file(
+        TEXT = f"**╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ⏤͟͟͞͞‌‌‌‌★**\n**┆**\n**┊◍ ʜᴇʏ : [{event.sender.first_name}] **\n**┆◍ ɪ ᴀᴍ : [{bot_name}](tg://user?id={bot_id}) **\n**┊**\n**┆● sᴀɴᴀᴛᴀɴɪ NETWORK :** `0.2`\n**┊● ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `8.2.5.1.01`\n**╰─────────────────────────**\n**──────────────────────────**\n**⦿ Oᴡɴᴇʀ - [🦋⃟‌AKASH 🌸](https://t.me/Akash_Daksh1c) | [🦋⃟‌AKASH 🌸](https://t.me/AD_Creation_Chatzone) **\n**──────────────────────────**\n**    ❖ Uᴘᴅᴀᴛᴇ's ⏤͟͟͞͞‌‌‌‌ [❖ ∣ Sᴀɴᴀᴛᴀɴɪ Network ∣ ❖](https://t.me/Sanatani_Network) **\n**──────────────────────────**"
+        await event.client.send_Full
                     event.chat_id,  
                     "https://telegra.ph//file/7cfeff721589b61a2f634.jpg",
                     caption=TEXT, 
